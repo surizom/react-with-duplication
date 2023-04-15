66be14c47e0f18b454a6892899ea17383b3a4b05
Moaad Fattali <moaad.fattali@ovrsea.com>
2023-04-15 18:23:46 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import codesandbox from "@gsimone/codesandbox-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), codesandbox()],
+  base: "/react-with-duplication",
   test: {
     globals: true,
     environment: "jsdom",
